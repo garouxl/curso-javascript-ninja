7646429e46bc085a507f30abec2eb15bd6a6efd6
Leandro Almeida <garou_xl@yahoo.com.br>
2018-08-13 11:07:09 -0300

    Challenge-25 revisado

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,93 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+
+/** Quebrador de texto, ira quebrar o texto de acordo com a altura do elemento
+ * O alvo deverá possuir um max-height configurado;
+ * podem ser inseridos mais parâmetros via global import no array de itens;
+ * FALSE irá quebrar por palavra e TRUE irá quebrar por letra;
+ */
+
+(function(targetArr) {
+  "use strict";
+  var ellipsis = (function() {
+    function textAddTitle(target) {
+      if (
+        target.getAttribute("title") !== target.innerHTML &&
+        target.getAttribute("title") !== null
+      )
+        return;
+      return target.setAttribute("title", target.innerHTML);
+    }
+
+    function textReset(target) {
+      return (target.innerHTML = target.getAttribute("title"));
+    }
+
+    function textSetOverFlow(target) {
+      return (target.style.overflow = "hidden");
+    }
+
+    function textVerifier(target) {
+      textAddTitle(target);
+      textReset(target);
+      textSetOverFlow(target);
+    }
+
+    function textSplitter(target) {
+      return target.innerHTML.split(" ");
+    }
+
+    function removeChar(target) {
+      return target.replace(/[\s,\.;?!]$/, "");
+    }
+
+    function halfWord(target) {
+      while (target.scrollHeight > target.offsetHeight) {
+        var cropped = target.textContent.substring(
+          0,
+          target.textContent.length - 5
+        );
+        target.textContent = removeChar(cropped) + " ...";
+      }
+      return target;
+    }
+
+    function fullWord(target, arr) {
+      while (target.scrollHeight > target.offsetHeight) {
+        arr.pop();
+        arr[arr.length - 1] = removeChar(arr[arr.length - 1]);
+        target.innerHTML = arr.join(" ") + " ...";
+      }
+      return target;
+    }
+
+    function loopHalfWord(target) {
+      if (target.scrollHeight <= target.offsetHeight) return false;
+      return loopHalfWord(halfWord(target));
+    }
+
+    return {
+      init: function(target, breakWord) {
+        try {
+          textVerifier(target);
+          breakWord ? halfWord(target) : fullWord(target, textSplitter(target));
+        } catch (error) {
+          console.warn("Houve um erro:", error);
+        }
+      }
+    };
+  })();
+
+  window.addEventListener("load", function(event) {
+    targetArr.forEach(function(item) {
+      ellipsis.init(item[0], item[1]);
+    });
+  });
+
+  window.addEventListener("resize", function(event) {
+    targetArr.forEach(function(item) {
+      ellipsis.init(item[0], item[1]);
+    });
+  });
+})([[document.querySelector(".ad-text"), true]]);
